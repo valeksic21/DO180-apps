8ef4da7ea99f80110a8668b6bfedabce2d23c4ae
Vjekoslav Aleksić <vjekoslav.aleksic@gmail.com>
2022-09-29 11:10:48 +0200

    add env.var

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "<p>New line</p>";
-print "<br>Additional message: " . getenv('APP_MSG’);
+print "<br>Additional message: " . getenv('APP_MSG');
 ?>
